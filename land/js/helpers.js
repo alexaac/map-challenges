bfccc744fda9e3d08120b55dcf97b79890529d9d
Alexa Cristina <alexa@maptheclouds.com>
2021-11-19 18:28:30 +0200

    feat: Day 17 - Land

--- land/js/helpers.js
+++ land/js/helpers.js
@@ -20,7 +20,7 @@ const drawLegend = async () => {
     .select('#legend')
     .append('svg')
     .attr('width', 250)
-    .attr('height', 900);
+    .attr('height', 800);
 
   let mapLegend = svg
     .append('g')
@@ -32,39 +32,39 @@ const drawLegend = async () => {
     .attr('x', 0)
     .attr('y', -20)
     .attr('width', 250)
-    .attr('height', 700)
+    .attr('height', 800)
     .style('fill', '#f9fbfa')
     .style('stroke', '#f9fbfa');
 
   mapLegend
     .append('text')
     .attr('x', 10)
-    .attr('y', 0)
+    .attr('y', 20)
     .style('fill', '#757575')
     .html('Land use');
 
-  const citiesLegend = mapLegend
+  const landLegend = mapLegend
     .append('g')
     .attr('class', 'map-legend')
-    .attr('transform', `translate(10, 20)`);
+    .attr('transform', `translate(10, 40)`);
 
-  citiesLegend
+  landLegend
     .selectAll('rect')
     .data(legendData[0])
     .enter()
     .append('rect')
     .attr('x', 0)
-    .attr('y', (d, i) => i * 20)
+    .attr('y', (d, i) => i * 17)
     .attr('width', 10)
     .attr('height', 5)
     .attr('fill', (d) => colorScale(d.code));
 
-  citiesLegend
+  landLegend
     .selectAll('text')
     .data(legendData[0])
     .enter()
     .append('text')
-    .attr('y', (d, i) => i * 20 + 5)
+    .attr('y', (d, i) => i * 17 + 5)
     .attr('x', 20)
     .style('fill', '#757575')
     .style('font-size', '9px')
